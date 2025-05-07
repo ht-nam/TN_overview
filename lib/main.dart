f8f6cee14b24e0304d9318d52586449374d61f46
GLB_NamHT <namht@globits.net>
2025-05-07 11:48:28 +0700

    Fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:tn/home_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future main() async{
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: "env");
   runApp(const MyApp());
 }
 
